e7ef8653898a89355e7b52deafd27e994417f896
jakerella <jordan@jordankasper.com>
2013-09-14 19:46:52 -0500

    Big rewrite of internals; changed inputs to --options; added ability to run multiple servers from single execution using config json file

--- README.md
+++ README.md
@@ -7,12 +7,13 @@ Run a simple proxy server with proper mime type support for static sites
 
 1. Clone the repo: `git clone git@github.com:jakerella/simple-proxy-server.git`
 2. Install (the very few) dependencies: `npm install`
-3. Run the server: `node /path/to/simple-proxy-server/simpleServer.js /path/to/project`
+3. Run the server: `node /path/to/simple-proxy-server/simpleServer.js --rootDir=/path/to/project`
 4. Hit your local site at http://localhost:8686
 
 ```
 node simpleServer.js [serverRootDirectory] [options]
 Options:
+  --rootDir   root directory for serving files [current working directory]
   --host      hostname to use ["localhost"]
   --port      port number to use [8686]
 ```
@@ -27,9 +28,32 @@ Simply add the host to your `/etc/hosts` file (`C:/Windows/System32/drivers/etc/
 ...
 ```
 
-Then start your server: `node /path/to/simple-proxy-server/simpleServer.js /path/to/project --host=somelocaldomain.com` and hit http://somelocaldomain.com:8686
+Then start your server: `node /path/to/simple-proxy-server/simpleServer.js --rootDir=/path/to/project --host=somelocaldomain.com` and hit http://somelocaldomain.com:8686
 
-(Or specify the port as well: `node /path/to/simple-proxy-server/simpleServer.js /path/to/project --host=somelocaldomain.com --port=9999` and hit http://somelocaldomain.com:9999)
+(Or specify the port as well: `node /path/to/simple-proxy-server/simpleServer.js --rootDir=/path/to/project --host=somelocaldomain.com --port=9999` and hit http://somelocaldomain.com:9999)
+
+
+### Specifying a config file
+
+The config file can be used to specify options for multiple hosts, all of which will be started using the same terminal session. Think of this as the equivalent of a vhosts file for other web server applications.
+
+To use it, just specify the `--config` option with the file path, and, of course, create the file!
+
+```js
+// In /path/to/config.json
+{
+    "hosts": [
+        { "rootDir": "/path/to/root/one", "host": "localhost", "port": 9999 },
+        { "rootDir": "/path/to/root/two", "host": "dev.example.com", "port": 5678 }
+    ]
+}
+```
+
+Then when starting up the server use:
+
+`~$ node /path/to/simple-proxy-server/simpleServer.js --config=/path/to/config.json`
+
+All log output will be in the same terminal window (from all servers). Pressing ctrl + c will kill all server sessions!
 
 
 ### Making it easier with a symlink
@@ -40,16 +64,14 @@ If you want to be able to run the server from anywhere, create a symlink:
 
 Then run `nserve` from any directory you want to serve up:
 
-```bash
-~$ cd /path/to/project/
-~$ nserve . somelocaldomain.com
-```
+`~$ nserve --rootDir=/path/to/project --host=somelocaldomain.com`
+
 
 ## Want to test it out?
 
 After cloning the repo (step 1), and installing dependencies (step 2), change to the server directory and do this:
 
-`node simpleServer.js ./test`
+`node simpleServer.js --rootDir=./test`
 
 Then hit http://localhost:8686 and you should see a picture of Hobbes (from the Calvin and Hobbes comic).
 
--- simpleServer.js
+++ simpleServer.js
@@ -6,86 +6,132 @@ var http = require("http"),
     fs = require("fs"),
     mime = require("mime"),
     
-    // app settings
-    proxyPort = 9000,
+    // app vars
+    options = {},
 
     // internal functions
-    startServer, getOptions;
+    main, prepareServer, startProxy;
 
 
-// Find an unused port for the proxy first...
-console.log("Finding unused port...".white);
-http.createServer().listen(function() {
-    proxyPort = this.address().port;
-    this.close(function() {
-        startServer();
-    });
-});
+main = function(args) {
+    var i, l, m;
 
-// This is the main log...
-// It is called after we have an unused port from above
-startServer = function() {
-    var options = getOptions();
+    console.log("Starting simple-proxy-server".white + "\nPress CTRL + C to shutdown".yellow.bold);
 
-    console.log("Starting simple-proxy-server...\n   ".white, options);
-
-    httpProxy.createServer(proxyPort, options.host).listen(parseInt(options.port, 10));
-
-    http.createServer(function(request, response) {
-      var uri = url.parse(request.url).pathname,
-          filename = path.join(options.rootDir, uri);
-      
-      console.log(("Handling " + request.method + " request: " + uri).white);
-
-      fs.exists(filename, function(exists) {
-        if(!exists) {
-          console.log(("File not found: " + filename).red.bold);
-          response.writeHead(404, {"Content-Type": "text/plain"});
-          response.write("404 Not Found\n");
-          response.end();
-          return;
-        }
-
-        if (fs.statSync(filename).isDirectory()) filename = path.join(filename, "/index.html");
-
-        fs.readFile(filename, "binary", function(err, file) {
-          if(err) {
-            console.log(("Unable to serve binary file: " + filename).red.bold);
-            response.writeHead(500, {"Content-Type": "text/plain"});
-            response.write(err + "\n");
-            response.end();
-            return;
-          }
-
-          response.writeHead(200, {"Content-Type": mime.lookup(filename)});
-          response.write(file, "binary");
-          response.end();
-        });
-      });
-    }).listen(proxyPort);
-
-    console.log(
-        "    simple-proxy-server running at => ".green + (options.host + ":" + options.port).green.bold + (" (proxying " + proxyPort + ")").grey +
-        ("\n    serving files from " + options.rootDir).white + 
-        "\nCTRL + C to shutdown".yellow.bold
-    );
-};
-
-getOptions = function() {
-    var options = {};
-
-    options.rootDir = process.argv[2] || process.cwd();
-    
-    for (i=3, l=process.argv.length; i<l; ++i) {
-        m = process.argv[i].match(/^\-\-([a-zA-Z0-9\-]+)\=(.+)$/);
+    // Prepare options
+    for (i=2, l=args.length; i<l; ++i) {
+        m = args[i].match(/^\-\-([a-zA-Z0-9\-]+)\=(.+)$/);
         if (m) {
             options[m[1]] = m[2];
         }
     }
 
+    if (options.config) {
+        fs.exists(options.config, function(exists) {
+            if(!exists) {
+                console.error(("ERROR: Config file does not exist! (" + options.config + ")").red.bold);
+                process.exit(404);
+            }
+
+            fs.readFile(options.config, "binary", function(err, file) {
+                var configJSON = {};
+
+                if(err) {
+                    console.error(("ERROR: Unable to read config file! (" + options.config + ")").red.bold);
+                    process.exit(500);
+                }
+
+                try {
+                    configJSON = JSON.parse(file);
+                } catch (e) {
+                    console.error(("ERROR: Unable to parse config file! " + e.message).red.bold);
+                    process.exit(500);
+                }
+
+                if (!Array.isArray(configJSON.hosts)) {
+                    console.error(("ERROR: config file does not have a 'hosts' array! ").red.bold);
+                    process.exit(400);
+                }
+
+                for (i=0, l=configJSON.hosts.length; i<l; ++i) {
+                    prepareServer(configJSON.hosts[i]);
+                }
+            });
+        });
+
+    } else {
+        // If no config file is specified use individual host options passed in (or defaults)
+
+        options.hosts = [
+            { "rootDir": options.rootDir, "host": options.host, "port": options.port }
+        ];
+
+        prepareServer(options.hosts[0]);
+    }
+};
+
+
+// This is the main functionality to start a single server
+prepareServer = function(options) {
+    options = (options || {});
+
+    if (!options.rootDir) { options.rootDir = (options.rootDir || process.cwd()); }
     if (!options.host) { options.host = "localhost"; }
     options.port = Number(options.port);
     if (!options.port) { options.port = 8686; }
 
-    return options;
+    console.log("Finding unused port for proxy...".white);
+    http.createServer().listen(function() {
+        options.proxyPort = this.address().port;
+        this.close(function() {
+            startProxy(options);
+        });
+    });
 };
+
+startProxy = function(options) {
+    console.log("Starting http-proxy...".white);
+    httpProxy.createServer(options.proxyPort, options.host).listen(parseInt(options.port, 10));
+
+    console.log("Starting http server...".white);
+    http.createServer(function(request, response) {
+        var uri = url.parse(request.url).pathname,
+            filename = path.join(options.rootDir, uri);
+      
+        console.log(("Handling " + request.method + " request to " + (options.host + ":" + options.port) + ": " + uri).white);
+
+        fs.exists(filename, function(exists) {
+            if(!exists) {
+                console.log(("File not found: " + filename).red.bold);
+                response.writeHead(404, {"Content-Type": "text/plain"});
+                response.write("404 Not Found\n");
+                response.end();
+                return;
+            }
+
+            if (fs.statSync(filename).isDirectory()) filename = path.join(filename, "/index.html");
+
+            fs.readFile(filename, "binary", function(err, file) {
+                if(err) {
+                    console.log(("Unable to serve binary file: " + filename).red.bold);
+                    response.writeHead(500, {"Content-Type": "text/plain"});
+                    response.write(err + "\n");
+                    response.end();
+                    return;
+                }
+
+                response.writeHead(200, {"Content-Type": mime.lookup(filename)});
+                response.write(file, "binary");
+                response.end();
+            });
+        });
+    }).listen(options.proxyPort);
+
+    console.log(
+        "    simple-proxy-server running at => ".green + (options.host + ":" + options.port).green.bold + (" (proxying " + options.proxyPort + ")").grey +
+        (" and serving files from " + options.rootDir).white
+    );
+};
+
+// Kick off the process
+main(process.argv);
